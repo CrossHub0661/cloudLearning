72dedf5f8f053e8c6cc1b6a184f622d4143d3fde
liuying <ying.liu@maxnerva.com>
2019-10-18 09:43:12 +0800

    eurekaServer test

--- first/src/main/java/com/learning/first/FirstApplication.java
+++ first/src/main/java/com/learning/first/FirstApplication.java
@@ -2,7 +2,6 @@ package com.learning.first;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
