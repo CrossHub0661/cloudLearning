f3968149658b58348feddbab978b9790010ce554
liuying <ying.liu@maxnerva.com>
2019-10-21 09:26:52 +0800

    date test for method

--- first/src/main/java/com/learning/first/DateTest.java
+++ first/src/main/java/com/learning/first/DateTest.java
@@ -1,18 +1,26 @@
 package com.learning.first;
 
+import org.springframework.web.bind.annotation.RestController;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
  * Created by liuying on 2019/10/18 9:47
  */
+@RestController
 public class DateTest {
 
     public final static SimpleDateFormat sdfDateTimeFormat = new SimpleDateFormat(
             "yyyy-MM-dd HH:mm:ss");
 
     public static void main(String[] args) {
-        System.out.println(DateTest.sdfDateTimeFormat.format(new Date()));
+        DateTest dateTest = new DateTest();
+        System.out.println(dateTest.getNowDate());
+    }
+
+    public String getNowDate() {
+        return DateTest.sdfDateTimeFormat.format(new Date());
     }
 
 }
