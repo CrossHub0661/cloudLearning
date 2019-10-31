0b866c6e149412638eee25a11d747abbd48aafac
liuying <ying.liu@maxnerva.com>
2019-10-31 09:43:28 +0800

    loop test

--- first/src/main/java/com/learning/first/LoopTest.java
+++ first/src/main/java/com/learning/first/LoopTest.java
@@ -10,14 +10,24 @@ public class LoopTest {
 
     public static void main(String[] args) {
         LoopTest loopTest = new LoopTest();
-        int result = loopTest.addOneTo();
-        System.out.println(result);
+        System.out.println(loopTest.addOneTo());
+        System.out.println(loopTest.addOneTo2());
     }
 
     public int addOneTo() {
         int all = 0;
-        for(int x = 1 ; x < 100 ; x++) {
+        for(int x = 1 ; x <= 100 ; x++) {
             all += x;
+        }
+        return all;
+    }
+
+    public int addOneTo2() {
+        int all = 0;
+        int x = 1;
+        while(x<=100) {
+            all += x;
+            x++;
         }
         return all;
     }
